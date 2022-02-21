e05fca46f391fa3bcaa1fcdad6b3372ac5e964fb
+ <+>
2022-02-21 13:03:17 -0500

    update footer with name and social icon links

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -5,8 +5,14 @@ const Footer = () => {
 
         <div>
             <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+            <div className="col-6">
+                <h5>Anna Stania Productions</h5>
+            </div>
+            <div className="col-6">
+                <SocialIcon url="https://www.linkedin.com/in/anna-stania/" />
+                <SocialIcon url="https://github.com/astania"/>
+                <SocialIcon url="https://medium.com/@annaestania"/>
+            </div>
             </div>
         </div>
 
