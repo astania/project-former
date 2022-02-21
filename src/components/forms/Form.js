f560032feac8df6ccfcd6647d0876647c530c4d2
+ <+>
2022-02-21 16:09:32 -0500

    begin rendering selected form

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,6 +1,36 @@
+import { useState } from "react";
 
-const Form = () => {
-    return <div></div>;
+const Form = ({ form }) => {
+    const questions = form.questions
+
+    const blankSubmission = ""
+
+    const [formResponses, setFormResponses] = useState(blankSubmission)
+  
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setFormResponses(event.target.value)
+        console.log(formResponses)
+    }
+
+    return (
+        <div>
+            <h1>{form.name}</h1>
+            <form>
+                {questions.map(question => {
+                    return (
+                        <label>
+                            {question.prompt}
+                                value={question.response}
+                                onChange={handleChange}
+                            ></input>
+                        </label>
+                    )
+                })}
+            </form>
+        </div >
+    );
 }
 
 
--- src/components/forms/FormCard.js
+++ src/components/forms/FormCard.js
@@ -1,20 +1,25 @@
 import Form from "./Form"
+import { useState } from "react"
 
 const FormCard = ({form}) => {
+    const [selectedForm, setSelectedForm] = useState(false)
+
     const handleSelectForm = () => {
         console.log(form)
-        return(
-            <Form form={form}/>
-        )
+        setSelectedForm(!selectedForm)
     }
 
 
     return (
+    <>   
     <div>
         <h4>{form.name}</h4>
         <h6>This form has {form.questions.length} questions</h6>
         <button onClick={handleSelectForm}>select this form</button>
-    </div>);
+    </div>
+    <div>{selectedForm ? <Form form={form}/> : null}</div>
+    </> 
+    );
 }
 
 
