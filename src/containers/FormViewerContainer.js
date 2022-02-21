b2ab26e43ac29aa77297135fc2b7caf2a352e80d
+ <+>
2022-02-21 15:28:57 -0500

    initial rendering of forms to FormViewerContainer

--- src/containers/FormViewerContainer.js
+++ src/containers/FormViewerContainer.js
@@ -1,8 +1,9 @@
+import FormCard from "../components/forms/FormCard";
 
 const FormViewerContainer = ({ forms }) => {
     return (
     <div>
-        {forms.map(form => <FormCard />)}
+        {forms.map(form => <FormCard name={form.name} questions={form.questions}/>)}
     </div>
     );
 }
