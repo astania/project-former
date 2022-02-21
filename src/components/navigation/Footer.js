a70ee4fdf5b8f55ff2654db3421cc857cb6037f9
+ <+>
2022-02-21 12:53:46 -0500

    download and import react social icons package

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,6 +1,16 @@
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
-    return <div></div>;
+    return (
+
+        <div>
+            <div className="row">
+            <div className="col-6"></div>
+            <div className="col-6"></div>
+            </div>
+        </div>
+
+    );
 }
 
 
