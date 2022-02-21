ac73badfd79d28ae2a57d36db1cbf61579bca2a1
+ <+>
2022-02-21 12:25:19 -0500

    add preliminary navigation bar links

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './navigation/NavBar';
 import Header from './navigation/Header';
+import Footer from './navigation/Footer';
 import FormBuilderContainer from '../containers/FormBuilderContainer';
 import FormViewerContainer from '../containers/FormViewerContainer';
-import HomePage from '../containers/HomePageContainer'
+import HomePageContainer from '../containers/HomePageContainer';
+import FormSubmissionsContainer from '../containers/FormSubmissionsContainer';
+import Form from './forms/Form';
 
 
 function App() {
@@ -14,22 +16,32 @@ function App() {
       <Router>
         <Header />
         <NavBar />
+
         <Switch>
           <Route path="/forms/new">
             <FormBuilderContainer />
           </Route>
+
           <Route path="/forms">
             <FormViewerContainer />
           </Route>
+
+          <Route path="/forms/:id">
+            <Form />
+          </Route>
+
           <Route path="/submissions">
             <FormSubmissionsContainer />
           </Route>
+
           <Route exact path="/">
             <HomePageContainer />
           </Route>
+
         </Switch>
       </Router>
-     
+
+    <Footer />
     </div>
   );
 }
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,7 +1,49 @@
+import { NavLink } from 'react-router-dom'
 
-function NavBar(){
+const style = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+}
+
+const activeStyle = {
+    fontWeight: "bolder",
+    color: "red"
+}
+
+function NavBar() {
     return (
-        <h1>Testing NavBar</h1>
+        <div>
+            <NavLink
+                activeStyle={activeStyle}
+                exact
+                style={style}
+                to="/"
+            >
+                Home
+            </NavLink>
+            <NavLink
+                activeStyle={activeStyle}
+                style={style}
+                to="/"
+            >
+                Build a Form
+            </NavLink>
+            <NavLink
+                activeStyle={activeStyle}
+                style={style}
+                to="/"
+            >
+                View Form
+            </NavLink>
+            <NavLink
+                activeStyle={activeStyle}
+                style={style}
+                to="/"
+            >
+                Submissions
+            </NavLink>
+        </div>
     )
 }
 
