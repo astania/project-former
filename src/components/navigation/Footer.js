4166934a7487fa19a96ff90e4183bedaff08452f
+ <+>
2022-02-21 14:54:58 -0500

    update footer icon size and style

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -4,14 +4,14 @@ const Footer = () => {
     return (
 
         <div>
-            <div className="row">
-            <div className="col-6">
+            <div className="row" style={{display:"flex", height:"50px", marginBottom:"-50px"}}>
+            <div className="col-6" style={{width:"50%", float: "left"}}>
                 <h5>Anna Stania Productions</h5>
             </div>
-            <div className="col-6">
-                <SocialIcon url="https://www.linkedin.com/in/anna-stania/" />
-                <SocialIcon url="https://github.com/astania"/>
-                <SocialIcon url="https://medium.com/@annaestania"/>
+            <div className="col-6" style={{width:"50%", float: "right"}}>
+                <SocialIcon url="https://www.linkedin.com/in/anna-stania/" style={{width:"1em", margin:".2em", padding:".2em"}} />
+                <SocialIcon url="https://github.com/astania" style={{width:"1em", margin:".2em", padding:".2em"}}/>
+                <SocialIcon url="https://medium.com/@annaestania" style={{width:"1em", margin:".2em", padding:".2em"}}/>
             </div>
             </div>
         </div>
