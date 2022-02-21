728fb65313d461f174c66f636d7e8910916518c2
+ <+>
2022-02-21 15:38:55 -0500

    update FormCard params, add button and eventListener to FormCard

--- src/components/forms/FormCard.js
+++ src/components/forms/FormCard.js
@@ -1,9 +1,19 @@
-const FormCard = ({name, questions}) => {
+import Form from "./Form"
+
+const FormCard = ({form}) => {
+    const handleSelectForm = () => {
+        console.log(form)
+        return(
+            <Form form={form}/>
+        )
+    }
+
+
     return (
     <div>
-        <h3>{name}</h3>
-        <h4>This form has {questions.length} questions</h4>
-
+        <h4>{form.name}</h4>
+        <h6>This form has {form.questions.length} questions</h6>
+        <button onClick={handleSelectForm}>select this form</button>
     </div>);
 }
 
--- src/containers/FormViewerContainer.js
+++ src/containers/FormViewerContainer.js
@@ -3,7 +3,7 @@ import FormCard from "../components/forms/FormCard";
 const FormViewerContainer = ({ forms }) => {
     return (
     <div>
-        {forms.map(form => <FormCard name={form.name} questions={form.questions}/>)}
+        {forms.map(form => <FormCard form={form} key={form.id}/>)}
     </div>
     );
 }
