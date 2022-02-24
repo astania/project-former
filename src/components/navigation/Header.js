29d593ca255f35a0cef455420352c011e8b596a5
+ <+>
2022-02-24 13:11:35 -0500

    add logo to header

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,6 +1,8 @@
 
+
 const Header = ({slogan, storeName}) => {
     return <div className="header">
+        <img src="https://imgur.com/a/i3Uzjop" alt="Project Former Logo" width="500" height="150"></img>
         <h1>{storeName}</h1>
         <h4><em>{slogan}</em></h4>
     </div>;
